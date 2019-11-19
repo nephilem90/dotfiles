015c356ef7dd5001ee306b597a7f3fa78e3474a5
furt <nephilem90@gmail.com>
2019-11-19 21:42:16 +0100

    aggiornamento powerline su zsh

--- after-reboot.sh
+++ after-reboot.sh
@@ -6,3 +6,14 @@ sudo npm install -g how-2
 echo "INSTALLO VUE-CLI"
 sudo npm install -g @vue/cli
 
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+git clone https://github.com/abertsch/Menlo-for-Powerline.git
+cd Menlo-for-Powerline
+sudo mv "Menlo for Powerline.ttf" /usr/share/fonts/
+sudo fc-cache -vf /usr/share/fonts/
+cd ..
+rm -rf Menlo-for-Powerline
+
+echo "imposta Meslo LG M for Powerline come font nel terminale e in vscode"
+echo "https://gist.github.com/kevin-smets/8568070"
